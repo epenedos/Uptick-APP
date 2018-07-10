abb46afd4ce50eeb8546bd967b539f78126d0881
Eduardo Penedos <epenedos@icloud.com>
2018-07-10 16:37:57 +0200

    Update mysqlConfig.js

--- config/mysqlConfig.js
+++ config/mysqlConfig.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
 var mysqlConnection = mysql.createConnection({
-   host: "127.0.0.1",
+   host: "172.16.154.35",
    user: "root",
    password: "nutanix/4u",
    database: "Uptick",
-   port:3306
+   port:32778
 });
 
 mysqlConnection.connect(function(err) {
